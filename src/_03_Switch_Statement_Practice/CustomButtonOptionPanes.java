23245e3d383c62ccc4082da2a098afb58b35012c
league <league@imac-15.attlocal.net>
2019-11-24 17:32:17 -0800

    stuff

--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -12,6 +12,32 @@ public class CustomButtonOptionPanes {
 		String choice = options[input];
 		
 		//use a switch statement to do something cool for each option
+		switch(choice) {
+		case "Sunday": 
+			System.out.println("7");
+			break;
+		case "Monday": 
+			System.out.println("1");
+			break;
+		case "Tuesday": 
+			System.out.println("2");
+			break;
+		case "Wednesday": 
+			System.out.println("3");
+			break;
+		case "Thursday": 
+			System.out.println("4");
+			break;
+		case "Friday": 
+			System.out.println("5");
+			break;
+		case "Saturday": 
+			System.out.println("6");
+			break;
+		default: 
+			System.out.println("Something went wrong :(");
+			break;
 		
+		}
 	}
 }
--- src/_05_Enum_Stuff/_00_Horoscope.java
+++ src/_05_Enum_Stuff/_00_Horoscope.java
@@ -8,7 +8,34 @@ public class _00_Horoscope {
 	
 	// 2. Write a method that takes in a Zodiac enum object and uses a JOPtionPane to display
 	//    a different horoscope based on the Zodiac's state.
-	
+	public void zodEnum(Zodiac current){
+		switch(current) {
+		case ARIES:
+			JOptionPane.showMessageDialog(null, "IDK");
+			break;
+		case TAURUS:
+			break;
+		case GEMINI:
+			break;
+		case CANCER:
+			break;
+		case LEO:
+			break;
+		case VIRGO:
+			break;
+		case LIBRA:
+			break;
+		case SCORPIO:
+			break;
+		case SAGITTARIUS:
+			break;
+		case CAPRICORN:
+			break;
+		case AQUARIUS:
+			break;
+		case PISCES:
+			break;
+	}
 	// 3. Make a main method to test your method
 	
 }
