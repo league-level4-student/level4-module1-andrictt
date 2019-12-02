08dcd2c55d07dc3af1df27431f17339b60ee9ad0
league <league@imac-10.attlocal.net>
2019-12-01 17:25:26 -0800

    enumstuff

--- src/_05_Enum_Stuff/_00_Horoscope.java
+++ src/_05_Enum_Stuff/_00_Horoscope.java
@@ -8,34 +8,50 @@ public class _00_Horoscope {
 	
 	// 2. Write a method that takes in a Zodiac enum object and uses a JOPtionPane to display
 	//    a different horoscope based on the Zodiac's state.
-	public void zodEnum(Zodiac current){
+	public static void zodEnum(Zodiac current){
 		switch(current) {
 		case ARIES:
 			JOptionPane.showMessageDialog(null, "IDK");
 			break;
 		case TAURUS:
+			JOptionPane.showMessageDialog(null, "IDK");
 			break;
 		case GEMINI:
+			JOptionPane.showMessageDialog(null, "IDK");
 			break;
 		case CANCER:
+			JOptionPane.showMessageDialog(null, "ho");
 			break;
 		case LEO:
+			JOptionPane.showMessageDialog(null, "h");
 			break;
 		case VIRGO:
+			JOptionPane.showMessageDialog(null, "d");
 			break;
 		case LIBRA:
+			JOptionPane.showMessageDialog(null, "s");
 			break;
 		case SCORPIO:
+			JOptionPane.showMessageDialog(null, "a");
 			break;
 		case SAGITTARIUS:
+			JOptionPane.showMessageDialog(null, "g");
 			break;
 		case CAPRICORN:
+			JOptionPane.showMessageDialog(null, "h");
 			break;
 		case AQUARIUS:
+			JOptionPane.showMessageDialog(null, "B");
 			break;
 		case PISCES:
+			JOptionPane.showMessageDialog(null, "j");
 			break;
+		}
 	}
 	// 3. Make a main method to test your method
+	public static void main(String[] args) {
+		Zodiac hey = Zodiac.CANCER;
+		zodEnum(hey);
+	}	
 	
 }
--- src/_06_Snake/_00_SnakeGame.java
+++ src/_06_Snake/_00_SnakeGame.java
@@ -75,7 +75,7 @@ public class _00_SnakeGame implements ActionListener, KeyListener {
 
 	public void startGame() {
 		//1. Save the instructions for the game in the following string variable.
-		String instructions = "";
+		String instructions = "Just click the arrow keys";
 		
 		String[] options = new String[] { "Expert", "Moderate", "Beginner" };
 		int input = JOptionPane.showOptionDialog(null, instructions, "Snake", 0, -1, null, options, 0);
@@ -85,8 +85,20 @@ public class _00_SnakeGame implements ActionListener, KeyListener {
 		//2. Use a switch statement to determine which difficulty was chosen.
 		//   Use timer.setDelay(delay) with different numbers to change the speed
 		//   of the game. The smaller the number, the faster it goes.
-
+		switch(choice) {
+		case "Expert":
+			timer.setDelay(100);
+			break;
+		case "Moderate":
+			timer.setDelay(500);
+			break;
+		case "Beginner":
+			timer.setDelay(1000);
+			break;
+			
+		}
 		//3. start the timer
+		timer.start();
 	}
 
 	public static void main(String[] args) {
@@ -96,14 +108,30 @@ public class _00_SnakeGame implements ActionListener, KeyListener {
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		//1. Use a switch statement on e.getKeyCode()
 		//   to determine which key was pressed.
-		
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_RIGHT:
+			snake.setDirection(Direction.RIGHT);
+			break;
+		case KeyEvent.VK_LEFT:
+			snake.setDirection(Direction.LEFT);
+			break;
+		case KeyEvent.VK_UP:
+			snake.setDirection(Direction.UP);
+			break;
+		case KeyEvent.VK_DOWN:
+			snake.setDirection(Direction.DOWN);
+			break;
+		case KeyEvent.VK_SPACE:
+			snake.feed();
+			break;
+		}
 		// if an arrow key is pressed, set the snake's 
 		// direction accordingly
 		
@@ -113,23 +141,35 @@ public class _00_SnakeGame implements ActionListener, KeyListener {
 
 	private void setFoodLocation() {
 		//1. Create a new Location object that is set to a random location
-		
+
+		Random rand = new Random();
+		Location loc = new Location(rand.nextInt(15), rand.nextInt(12));
 		//2. set the foodLocation variable equal to the Location object you just created.
 		//   use the snake's isLocationOnSnake method to make sure you don't put the food on the snake
-		
+		foodLocation = loc;
+		snake.isLocationOnSnake(foodLocation);
 	}
 
 	private void gameOver() {
 		
 		//1. stop the timer
-		
+		timer.stop();
 		//2. tell the user their snake is dead
-		
+		JOptionPane.showMessageDialog(null, "You died lul");
 		//3. ask them if they want to play again.
-		
+		String[] options = new String[] { "Yes", "No" };
+		int input = JOptionPane.showOptionDialog(null, "Play Again?", "Snake", 0, -1, null, options, 0);
 		//4. if they want to play again
 		//   reset the snake and the food and start the timer
 		//   else, exit the game
+		String choice = options[input];
+		switch(choice) {
+		case "Yes":
+			snake.reset(foodLocation);
+			break;
+		case "No":
+			System.exit(0);
+		}
 		
 	}
 
@@ -141,13 +181,19 @@ public class _00_SnakeGame implements ActionListener, KeyListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		//1. update the snake
-
+		snake.update();
 		//2. if the snake is colliding with its own body 
 		//   or if the snake is out of bounds, call gameOver
-
+		if(snake.isHeadCollidingWithBody() || snake.isOutOfBounds()) {
+			gameOver();
+		}
 		//3. if the location of the head is equal to the location of the food,
 		// 	 feed the snake and set the food location
-
+		if(snake.getHeadLocation() == foodLocation) {
+			snake.feed();
+			setFoodLocation();
+		}
 		//4. call panel.repaint();
+		panel.repaint();
 	}
 }
